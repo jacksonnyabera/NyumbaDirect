8286810582cea51cb599b82cb849d77a885d669c
Jackson Nyabera <maobe928@gmail.com>
2026-09-07 01:07:35 +0300

    Fix production CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,6 +22,8 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5173",
         "http://localhost:5173",
+        "http://127.0.0.1:5174",
+        "http://localhost:5174",
         "https://nyumba-direct-rust.vercel.app",
     ],
     allow_credentials=True,
